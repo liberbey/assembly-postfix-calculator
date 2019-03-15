5be609bced8093b799cbb2c052a649282a1fa313
Ahmet Emir <37846781+liberbey@users.noreply.github.com>
2019-03-15 17:50:07 +0300

    Update calculator.asm

--- calculator.asm
+++ calculator.asm
@@ -3,120 +3,151 @@ code segment:
   mov ax,0h
   mov cx,0h
   mov dx,0h
-  call read ;
-  ;call print
-  ;ad to stack one by one
-  ;if seen any operation, go to operation
-  ;do it again untill enter
-  ;take last num from stack
-  ;print it
-  ;push 0010h
-  ;push 1000h
-  ;jmp addition
+  call read
 
-read:
-  mov ah,01h  ;
-  int 21h     ; okudugumuz sayi AL'de
-  cmp al,2Bh
-  je addition
-  cmp al,20h
-  je pushvalue
-  cmp al,0Dh  ;
-  je print
-  sub al,'0'  ;
-  mov cx,0h
-  mov cl,al
-  mov ax,10h ; bx okunan sayıyı biriktirdiğimiz yer
-  mul bx   ;  al deki değer ile çarpıyor, ax'e yazıyor
-  add cx,ax   ;
-  mov bx,cx
-  jmp read    ; bx önceden sıfırlanıyor.
+letter_input:
+  sub al,"A"
+  add al, 10d
+  jmp read_cont
 
 pushvalue:
+ cmp bx, 0h
+ je read
  push bx
  mov bx,0h
  jmp read
-
-print:  ; top of stack ekrana yazdır.
-  pop cx          ; cx = 5555
-  mov ax, cx
-  mov dx, 0h
-  mov bx, 10h
-  div bx          ; ax = 0005,  dx = 0555
-  push dx
-  mov dx,0h
-  div bx
-  push dx
-  mov dx,0h
-  div bx
-  push dx
-  mov dx,0h
-  div bx
-  push dx
-  mov dx,0h
-  mov ah,02h
-  pop dx
-  add dl,'0'  ;
-  int 21h
-  pop dx
-  add dl,'0'  ;
-  int 21h
-  pop dx
-  add dl,'0'  ;
-  int 21
-  pop dx
-  add dl,'0'  ;
-  int 21
-
-
-  mov dl, al      ; ax = 0005,  dx = 0555
-  add dl,'0'
-  mov ah,02h
-  int 21h
-
-  mov dx,cx
-  mov bx, 100h
-  mov ax, dx
-  mov dx, 0h      ; ax = 0555,  dx = 0000
-  div bx          ;
-  mov cx, dx
-  mov dl, al
-  add dl,'0'
-  mov ah,02h
-  int 21h
-  int 20h
-  ;mov dx,cx
-  ;mov bx, 10h
-  ;mov ax, dx
-  ;mov dx, 0h      ; ax = 0555,  dx = 0000
-  ;div bx          ;
-  ;mov cx, dx
-  ;mov dl, al
-  ;add dl,'0'
-  ;mov ah,02h
-  ;int 21h
-
-  ;mov dx,cx
-  ;mov ax, dx     ;
-  ;mov dl, al
-  ;add dl,'0'
-  ;mov ah,02h
-  ;int 21h
-  ;int 20h
 
 addition:
   pop ax
   pop bx
   add ax,bx
   push ax
+  mov bx, 0h
   jmp read
-
 
 myret:
   ret
-;multiplication:
-;division:
-;xor:
-;and:
-;or:
+
+multiplication:
+  mov dx,0h
+  pop ax
+  pop bx
+  mul bx
+  push ax
+  mov bx, 0h
+  jmp read
+
+division:
+  mov dx,0h
+  pop bx
+  pop ax
+  div bx
+  push ax
+  mov bx, 0h
+  jmp read
+
+  read:
+    mov ah,01h  ;
+    int 21h     ; okudugumuz sayi AL'de
+    cmp al,2Bh
+    je addition
+    cmp al,20h
+    je pushvalue
+    cmp al,0Dh  ;
+    je print
+    cmp al,2Ah  ;
+    je multiplication
+    cmp al,2Fh  ;
+    je division
+    cmp al,5Eh  ;
+    je myxor
+    cmp al,26h  ;
+    je myand
+    cmp al,7Ch  ;
+    je myor
+    cmp al,40h
+    jg letter_input
+    sub al,'0' ;
+    jmp read_cont
+
+  read_cont:
+    mov cx,0h
+    mov cl,al
+    mov ax,10h ; bx okunan sayıyı biriktirdiğimiz yer
+    mul bx   ;  al deki değer ile çarpıyor, ax'e yazıyor
+    add cx,ax   ;
+    mov bx,cx
+    jmp read    ; bx önceden sıfırlanıyor.
+
+myxor:
+  pop ax
+  pop bx
+  xor ax,bx
+  push ax
+  mov bx, 0h
+  jmp read
+
+myand:
+  pop ax
+  pop bx
+  and ax,bx
+  push ax
+  mov bx, 0h
+  jmp read
+
+myor:
+  pop ax
+  pop bx
+  or ax,bx
+  push ax
+  mov bx, 0h
+  jmp read
+
+print:
+  mov ah,02h
+  mov dl, 10d
+  int 21h
+  pop ax
+  mov bx,10h
+  mov cx,0h
+  mov dx,0h
+  div bx
+  push dx
+  inc cx
+  cmp ax,0h
+  je print_finish
+  mov dx,0h
+  div bx
+  push dx
+  inc cx
+  cmp ax,0h
+  je print_finish
+  mov dx,0h
+  div bx
+  push dx
+  inc cx
+  cmp ax,0h
+  je print_finish
+  push ax
+  inc cx
+  jmp print_finish
+
+
+print_finish:
+  mov ah,02h
+  pop dx
+  cmp dl,9h
+  jg letter_output
+  add dl, '0'
+print_cont:
+  int 21h
+  dec cx
+  jnz print_finish
+  int 20h
+
+letter_output:
+  sub dl, 10d
+  add dl, "A"
+  jmp print_cont
+
 code ends
