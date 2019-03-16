45f5fb62ef991733edd9e132bf68903d453293ef
Ahmet Emir <37846781+liberbey@users.noreply.github.com>
2019-03-16 11:53:59 +0300

    Update calculator.asm

--- calculator.asm
+++ calculator.asm
@@ -2,104 +2,104 @@ code segment:
   mov bx,0h
   mov ax,0h
   mov cx,0h
-  mov dx,0h
-  call read
+  mov dx,0h ; all registers are 0
+  call read ; calls read
 
 letter_input:
   sub al,"A"
-  add al, 10d
+  add al, 10d ; to convert the value
   jmp read_cont
 
 pushvalue:
-  cmp bx, 0h
-  je read
-  push bx
-  mov bx,0h
-  jmp read
+ cmp bx, 0h ; if the last input was an operation, go back to read
+ je read
+ push bx ; push the last given input to the stack
+ mov bx,0h ; make bx 0, because it is used in taking input
+ jmp read
 
 addition:
-  pop ax
-  pop bx
-  add ax,bx
-  push ax
+  pop ax ; take the top of stack
+  pop bx ; take the top of stack
+  add ax,bx ; add them
+  push ax ; push it to stack
   mov bx, 0h
   jmp read
 
-myret:
+myret: ; return label
   ret
 
 multiplication:
-  mov dx,0h
-  pop ax
-  pop bx
-  mul bx
-  push ax
+  mov dx,0h ; make dx zero, just in case
+  pop ax ; take the top of stack
+  pop bx ; take the top of stack
+  mul bx ; multiply them
+  push ax ; push back to stack
   mov bx, 0h
   jmp read
 
 division:
-  mov dx,0h
-  pop bx
-  pop ax
-  div bx
-  push ax
+  mov dx,0h ; make dx zero, just in case
+  pop bx ; take the top of stack
+  pop ax ; take the top of stack
+  div bx ; divide ax by bx
+  push ax ; push the result to the stack
   mov bx, 0h
   jmp read
 
 read:
-  mov ah,01h  ;
-  int 21h     ; okudugumuz sayi AL'de
-  cmp al,2Bh
+  mov ah,01h  ; to take inpur
+  int 21h     ; takes input
+  cmp al,2Bh  ; compare with +
   je addition
-  cmp al,20h
+  cmp al,20h  ; compare with ' '
   je pushvalue
-  cmp al,0Dh  ;
+  cmp al,0Dh  ; compare with enter
   je print
-  cmp al,2Ah  ;
+  cmp al,2Ah  ; compare with *
   je multiplication
-  cmp al,2Fh  ;
+  cmp al,2Fh  ; compare with /
   je division
-  cmp al,5Eh  ;
+  cmp al,5Eh  ; compare with ^
   je myxor
-  cmp al,26h  ;
+  cmp al,26h  ; compare with &
   je myand
-  cmp al,7Ch  ;
+  cmp al,7Ch  ; compare with |
   je myor
-  cmp al,40h
+  cmp al,40h  ; compare with 40h to understand of the input is a letter or number
   jg letter_input
-  sub al,'0' ;
+  sub al,'0'  ; if it is not a letter, subtract 0 to convert its value
   jmp read_cont
 
 read_cont:
   mov cx,0h
-  mov cl,al
-  mov ax,10h ; bx okunan sayıyı biriktirdiğimiz yer
-  mul bx   ;  al deki değer ile çarpıyor, ax'e yazıyor
-  add cx,ax   ;
-  mov bx,cx
-  jmp read    ; bx önceden sıfırlanıyor.
+  mov cl,al  ; put the input in cl
+  mov ax,10h
+  mul bx     ; if the given input is digit of a large number, multiply the first digits with 10h, if it is not the result will be 0
+  add cx,ax  ; then add the input to the result of multiplication
+  mov bx,cx  ; move the input to bx
+  jmp read   ; back to read
 
 myxor:
-  pop ax
-  pop bx
-  xor ax,bx
-  push ax
+  pop ax ; take the top of stack
+  pop bx ; take the top of stack
+  xor ax,bx ; xor operation
+  push ax ; push the result in stack
   mov bx, 0h
   jmp read
 
 myand:
-  pop ax
-  pop bx
-  and ax,bx
-  push ax
+  pop ax ; take the top of stack
+  pop bx ; take the top of stack
+  and ax,bx ; and operation
+  push ax ; push the result in stack
   mov bx, 0h
   jmp read
 
 myor:
-  pop ax
-  pop bx
-  or ax,bx
-  push ax
+  pop ax ; take the top of stack
+  pop bx ; take the top of stack
+  or ax,bx ; or operation
+  push ax ; push the result in stack
   mov bx, 0h
   jmp read
 
@@ -139,7 +139,7 @@ print_finish:
   cmp dl,9h
   jg letter_output
   add dl, '0'
-  print_cont:
+print_cont:
   int 21h
   dec cx
   jnz print_finish
